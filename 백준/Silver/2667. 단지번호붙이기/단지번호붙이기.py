434bc28bbef7e1b6b121ee4ad37c39c88b81f78d
in-yh <109324632+in-yh@users.noreply.github.com>
2023-08-28 22:03:25 +0900

    [Silver I] Title: 단지번호붙이기, Time: 44 ms, Memory: 31348 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.py
@@ -4,12 +4,12 @@ home = [list(map(int, input())) for _ in range(N)]
 # dfs
 def dfs(p, q):
     global answer
+    visited[p][q] = 1
+    answer += 1
     for di, dj in [[0, 1],[1, 0],[0, -1],[-1, 0]]:
         ni, nj = p + di, q + dj
         if 0<=ni<N and 0<=nj<N and home[ni][nj]==1 and visited[ni][nj]==0:
-            visited[ni][nj] = visited[p][q] + 1
             dfs(ni, nj)
-            answer += 1
     # visited[p][q] = 0 # 지워줘야 들어올 수 있음!!
     return
 
@@ -18,12 +18,11 @@ result = []
 for si in range(N):
     for sj in range(N):
         if visited[si][sj]==0 and home[si][sj]==1:
-            answer = 1
-            visited[si][sj] = 1
+            answer = 0
             dfs(si, sj)
             result.append(answer)
-result.sort()
 
+result.sort()
 print(len(result))
 for k in range(len(result)):
     print(result[k])
